4c32ed784dd5555a4077a5f93ad2118849a27602
jsrn <james.srn@gmail.com>
2014-11-19 22:14:12 +0000

    space

--- app/controllers/checklist_controller.rb
+++ app/controllers/checklist_controller.rb
@@ -2,7 +2,7 @@ class ChecklistController < ApplicationController
   def index;end
 
   def new
-    checklist = Checklist.new({ token: SecureRandom.hex})
+    checklist = Checklist.new({ token: SecureRandom.hex })
 
     if checklist.save
       redirect_to(checklist)
